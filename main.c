a8251f1427e2d60d8b3e41afd74899bea1420070
Sanghoon Lee <salee2@c10r1s3.42seoul.kr>
2022-10-13 18:16:47 +0900

    mandatroy need output redirection

--- main.c
+++ main.c
@@ -41,7 +41,6 @@ int main(int argc, char **argv, char **envp)
 {
 	int pipe_fds[2];
 	pid_t pid;
-	int wstatus;
 
 	if (pipe(pipe_fds)) {
 		perror("pipe()");
@@ -55,76 +54,10 @@ int main(int argc, char **argv, char **envp)
 	}
 	else if (pid > 0)	// parent
 	{
-		close(pipe_fds[READER_FD]);
-		//write
-		const int fd = open(argv[1], O_RDONLY);
-		if(dup2(fd, STDIN_FILENO) == -1)
-		{
-			perror("dup2()");
-			exit(-1);
-		}
-		close(fd);
-		close(pipe_fds[WRITER_FD]);
-		pid = wait(&wstatus);
-	}
-	else	// child
-	{
-		close(pipe_fds[WRITER_FD]);
-		dup2(pipe_fds[READER_FD], STDIN_FILENO);
-		close(pipe_fds[READER_FD]);
-
-		char **arg_vec = ft_split(argv[2], ' ');
-
-		char **file_list = get_file_list("PATH", envp, arg_vec[0]);
-
-		int i = -1;
-		while (file_list[++i])
-		{
-			if (access(file_list[i], F_OK) == 0)
-			{
-				if (execve(file_list[i], arg_vec, envp) == -1)
-				{
-					perror("Could not execute execve");
-				}
-			}
-		}
-	}
-	(void) argc;
-	return 0;
-}
-
-int main2(int argc, char **argv, char **envp)
-{
-	int pipe_fds[2];
-	pid_t pid;
-//	int wstatus;
-
-	if (pipe(pipe_fds)) {
-		perror("pipe()");
-		return -1;
-	}
-	pid = fork();
-	if (pid < 0)
-	{
-		perror("fork()");
-		exit(1);
-	}
-	else if (pid > 0)	// parent
-	{
-		/*
-		close(pipe_fds[READER_FD]);
-		//write
-		const int fd = open(argv[1], O_RDONLY);
-		if(dup2(fd, STDIN_FILENO) == -1)
-		{
-			perror("dup2()");
-			exit(-1);
-		}
-		close(fd);
-		close(pipe_fds[WRITER_FD]);
-		*/
 		wait(0);
+
 		close(pipe_fds[WRITER_FD]);
+		//read
 		dup2(pipe_fds[READER_FD], STDIN_FILENO);
 		char **arg_vec = ft_split(argv[3], ' ');
 		char **file_list = get_file_list("PATH", envp, arg_vec[0]);
@@ -134,17 +67,18 @@ int main2(int argc, char **argv, char **envp)
 			if (access(file_list[i], F_OK) == 0)
 			{
 				if (execve(file_list[i], arg_vec, envp) == -1)
-//					if (execve(file_list[i], arg_vec, envp) == -1)
 				{
 					perror("Could not execute execve");
 				}
 			}
 		}
+
+		close(pipe_fds[WRITER_FD]);
 	}
 	else	// child
 	{
+		// input redirection
 		close(pipe_fds[READER_FD]);
-		//write
 		const int fd = open(argv[1], O_RDONLY);
 		if(dup2(fd, STDIN_FILENO) == -1)
 		{
@@ -152,6 +86,7 @@ int main2(int argc, char **argv, char **envp)
 			exit(-1);
 		}
 		close(fd);
+		//write
 		dup2(pipe_fds[WRITER_FD], STDOUT_FILENO);
 		char **arg_vec = ft_split(argv[2], ' ');
 		char **file_list = get_file_list("PATH", envp, arg_vec[0]);
@@ -161,12 +96,10 @@ int main2(int argc, char **argv, char **envp)
 			if (access(file_list[i], F_OK) == 0)
 			{
 				if (execve(file_list[i], arg_vec, envp) == -1)
-//					if (execve(file_list[i], arg_vec, envp) == -1)
-				{
 					perror("Could not execute execve");
-				}
 			}
 		}
+		close(pipe_fds[WRITER_FD]);
 	}
 	(void) argc;
 	return 0;
