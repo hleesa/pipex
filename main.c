cf3d1aab6b4dc1c1584ef34e9cdaf32e2228ef4f
Sanghoon Lee <salee2@c1r8s7.42seoul.kr>
2022-10-15 15:32:32 +0900

    mandatroy fin

--- main.c
+++ main.c
@@ -14,31 +14,39 @@
 
 int main(int argc, char **argv, char **envp)
 {
-	int *pipe_fds;
-	pid_t pid;
+	pid_t	pid;
+	int		*pipe_fds;
 
 	exit_if_invalid_arg(argc);
-	pipe_fds = make_pipe();
 	pid = fork();
 	if (pid < 0)
 		exit_fork_error();
 	else if (pid > 0)	// parent
-	{
-		close(pipe_fds[WRITER_FD]);
 		wait(0);
-		output_redirection(argv[4]);
-		dup2(pipe_fds[READER_FD], STDIN_FILENO);
-		close(pipe_fds[READER_FD]);
-		run_execve(argv[3], envp);
-	}
 	else	// child
 	{
-		close(pipe_fds[READER_FD]);
-		input_redirection(argv[1]);
-		dup2(pipe_fds[WRITER_FD], STDOUT_FILENO);
-		close(pipe_fds[WRITER_FD]);
-		run_execve(argv[2], envp);
+		pipe_fds = make_pipe();
+		pid = fork();
+		if(pid < 0)
+			exit_fork_error();
+		else if(pid > 0) // parent
+		{
+			input_redirection(argv[1]);
+			close(pipe_fds[READER_FD]);
+			dup2(pipe_fds[WRITER_FD], STDOUT_FILENO);
+			close(pipe_fds[WRITER_FD]);
+			free(pipe_fds);
+			run_execve(argv[2], envp);
+		}
+		else	// child
+		{
+			output_redirection(argv[4]);
+			close(pipe_fds[WRITER_FD]);
+			dup2(pipe_fds[READER_FD], STDIN_FILENO);
+			close(pipe_fds[READER_FD]);
+			free(pipe_fds);
+			run_execve(argv[3], envp);
+		}
 	}
-	free(pipe_fds);
 	return 0;
 }
